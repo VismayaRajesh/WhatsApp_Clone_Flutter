4dc1ce4c3ad06be762068c240255f049fb857b93
VismayaRajesh <vismayar3110@gmail.com>
2024-11-20 16:07:36 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone_flutter/Community.dart';
-import 'package:whatsapp_clone_flutter/call.dart';
-import 'package:whatsapp_clone_flutter/contact_list.dart';
-import 'package:whatsapp_clone_flutter/splash.dart';
-import 'package:whatsapp_clone_flutter/whatsapp_chat.dart';
+import 'package:whatsapp_clone_flutter/screens/splash.dart';
 
-import 'chatview.dart';
+import 'screens/chatview.dart';
 
 void main() {
   runApp(const MyApp());
